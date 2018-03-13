bb06067843869f3d4811d94ce6ca464eada5dce6
cb_Lian <787162506@qq.com>
2018-03-13 23:54:54 +0800

    架构改变

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: cb_Lian
+# Mail: 787162506@qq.com
+# Created Time:  2018-3-13 19:59:34
+#############################################
+
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "TradeInterface",
+	py_modules = ['TradeInterface'],
+    version = "0.1.4",
+    keywords = ("pip", "TradeInterface","trade",'haizhi'),
+    description = "a interface of simulate-trading stocks",
+    long_description = "a interface of simulate-trading stocks",
+    license = "MIT Licence",
+    url = "https://github.com/Inistlwq/TradeInterface",
+    author = "cb_Lian",
+    author_email = "787162506@qq.com",
+
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any"
+)
