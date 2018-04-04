b8f00704b75dab36fcf10898551b5518d224347f
cb_Lian <787162506@qq.com>
2018-04-04 10:29:04 +0800

    更新接口函数

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # TradeInterface
 海知平台模拟炒股接口
+
+#使用样例
+
+from TradeInterface1 import Trade
+trade = Trade(userid='**', password='******')
+
+trade.set_stock_dic(
+    {'buy_sell': 'sell', 'code': '000006', 'volume': '100', 'price': '1',
+     'price_type': 'now_price', 'effect_term': '2'}
+)
+
+# 进行交易请求
+res = trade.buy()  # 买入
+# res = trade.sell()  # 卖出
+# res = trade.query_records(startday="2018-4-4", endday="2018-04-05")  # 委托查询
+# res = trade.cancel_order(pre_id="201804040948554657")  # 撤单
+# res = trade.query_history_records(startday="2018-04-04", endday="2018-04-05")  # 历史交割查询
+# res = trade.query_profit()  # 资产查询
+
+print(res)
--- TradeInterface/TradeInterface.py
+++ TradeInterface/TradeInterface.py
@@ -20,34 +20,64 @@ if sys.getdefaultencoding() != 'utf-8':
 
 class Trade:
     """
-    参数说明：1.userId，用户Id，需在海知平台注册获得，默认'',必须设置
+    参数说明: 1.userId，用户Id，需在海知平台注册获得，默认'',必须设置
               2.password，用户密码，海知平台注册时用户所设，默认'',必须设置
-              3.cash_ratio 预留现金的比例，0到1之间
-              4.buy_sell，买卖操作,'buy' 或 'sell' , 默认'',必须设置
-              5.buy_price，设置买入某只股票的价格，默认'' ，若不设置，将会按照市价(买一)进行操作
-              6.sell_price，设置卖掉某只股票的价格，默认'' ，若不设置，将会按照市价(卖一)进行操作
-              7.volume，设置买卖股票的数量，默认‘average_all’,若不设置，默认买入时可用剩余资金在可购买股票中平均分配，
-                        卖出时全部卖出，若设置数量，则按所能实际买卖的数量与设置的数量取最小值交易
-              8.code，买卖股票的代码，默认'',必须设置
+              3.buy_sell 取值为'buy'、'sell',必须设置
+              4.code，买卖股票代码,必须设置
+              5.volume，买卖数量，必须设置
+              6.price，买卖的价格,在price_type 为limit_price时必须设置
+              7.price_type，买卖价格的类型，取值为‘limit_price’、‘now_price’，必须设置
+              8.effect_term，委托的期限，取值为1,2,3……，1代表今天，2代表明天，其他依此类推
 
     """
 
-    def __init__(self, userid='', password='', cash_ratio=''):
-        self.send_dic = {'userid': '', 'password': '', 'cash_ratio':'', 'stock_dic_ls': list()}
+    def __init__(self, userid='', password=''):
+        self.prefix = "http://www.haizhilicai.com/"
+        # self.prefix = "http://192.168.0.136/"
+        self.trade_url = self.prefix + 'Tradeinterface/get_tradeinfo'
+        self.prof_info_url = self.prefix + 'Tradeinterface/get_profit_info'
+        self.query_records_url = self.prefix + 'Tradeinterface/get_rocords_info'
+        self.cancel_url = self.prefix + 'Tradeinterface/cancel_orders'
+        self.query_history_url = self.prefix + 'Tradeinterface/get_history_info'
+        self.stock_encode_name = 'tradeinfo'
+        self.query_pro_encode_name = 'query_info'
+        self.cancel_encode_name = 'cancel_info'
+        self.send_dic = {'userid': '', 'password': '', 'buy_sell_type':'', 'stock_dic': {}}
         self.set_userid(userid)
         self.set_password(password)
-        self.set_cash_ratio(cash_ratio)
-        self.url = 'http://www.haizhilicai.com/Tradeinterface/get_tradeinfo'
 
-    def set_cash_ratio(self, cash_ratio):
-        cash_ratio = str(cash_ratio)
-        if self.isnumber(cash_ratio) is True:
-            self.send_dic['cash_ratio'] = cash_ratio
-            return True
-        else:
-            return False
+    # 买交易
+    def buy(self):
+        self.send_dic['buy_sell_type'] = 'buy'
+        return self.http_post(self.send_dic, self.stock_encode_name, self.trade_url)
 
-    # set userid of user of haizhi licai
+    # 卖交易
+    def sell(self):
+        self.send_dic['buy_sell_type'] = 'sell'
+        return self.http_post(self.send_dic, self.stock_encode_name, self.trade_url)
+
+    # 资产和持仓情况
+    def query_profit(self):
+        return self.http_post(self.send_dic, self.query_pro_encode_name, self.prof_info_url)
+
+    # 委托记录
+    def query_records(self, startday="", endday=""):
+        self.send_dic['start_day'] = startday
+        self.send_dic['end_day'] = endday
+        return self.http_post(self.send_dic, self.query_pro_encode_name, self.query_records_url)
+
+    # 撤单
+    def cancel_order(self, pre_id=""):
+        self.send_dic['pre_id'] = pre_id
+        return self.http_post(self.send_dic, self.cancel_encode_name, self.cancel_url)
+
+    # 历史交割
+    def query_history_records(self, startday="", endday=""):
+        self.send_dic['start_day'] = startday
+        self.send_dic['end_day'] = endday
+        return self.http_post(self.send_dic, self.query_pro_encode_name, self.query_history_url)
+
+    # 设置用户id
     def set_userid(self, userid=''):
         userid = str(userid)
         if userid.strip() != '':
@@ -56,7 +86,7 @@ class Trade:
         else:
             return False
 
-    # set password of haizhi licai
+    # 设置用户密码
     def set_password(self, password=''):
         password = str(password)
         if password.strip() != '':
@@ -65,21 +95,23 @@ class Trade:
         else:
             return False
 
-    # set stocks_ls of your own stocks,the type is list,and each elemnet of it is dictionary
-    def set_stock_dic_ls(self, stock_dic_ls=list()):
-        if len(stock_dic_ls) > 0:
-            self.send_dic['stock_dic_ls'] = stock_dic_ls
+    # 设置买卖股票列表
+    def set_stock_dic(self, stock_dic={}):
+        if len(stock_dic) > 0:
+            self.send_dic['stock_dic'] = stock_dic
             return True
         else:
             return False
 
-    # return the template of stock_dic
-    def get_stock_dic(self):
-        stock_dic = {'buy_sell': '', 'buy_price': '', 'sell_price': '', 'volume': 'average_all', 'code': ''}
+    # 设置买卖股票列表模板
+    @staticmethod
+    def get_stock_dic():
+        stock_dic = {'price': '', 'price_type': '', 'effect_term': '', 'buy_sell': '', 'volume': '', 'code': ''}
         return stock_dic
 
     # 实现md5加密
-    def md5encryption(self, password):
+    @staticmethod
+    def md5encryption(password):
         if password.strip() != '':
             md = hashlib.md5()
             md.update(password)
@@ -87,82 +119,19 @@ class Trade:
         else:
             return ''
 
-    # 判断字符串是否是浮点数
-    def isnumber(self, astring):
-        try:
-            float(astring)
-            return True
-        except:
-            return False
-
-    # 验证现金比例是否是大于0小于1的浮点数
-    def validate_cash_ratio(self, cash_ratio):
-        if self.isnumber(cash_ratio) is True:
-            if float(cash_ratio) < 0.0 or float(cash_ratio) > 1.0:
-                return "cash_ratio should be between 0 and 1 !"
-            else:
-                return True
-        else:
-            return "cash_ratio should be float!"
-
-    # post the request to haizhi licai,and return the result
-    def http_post(self):
-        # 用户id为空
-        if self.send_dic['userid'] == '':
-            return "userid attribute can't be a blank string!"
-        # 用户密码为空
-        if self.send_dic['password'] == '':
-            return "password attribute can't be a blank string!"
-        # 验证现金比例是否大于0小于1的浮点数
-        if self.validate_cash_ratio(self.send_dic['cash_ratio'])is not True:
-            return self.validate_cash_ratio(self.send_dic['cash_ratio'])
-        temp_stock_dic_ls = self.send_dic['stock_dic_ls']
-        # 用户的股票信息为空
-        if len(temp_stock_dic_ls) == 0:
-            return "stock info must be needed!"
-        else:  # 检验每条股票单的键和值否正确
-            for stock_dic in temp_stock_dic_ls:
-                tempset = set(['buy_sell', 'code', 'volume', 'buy_price', 'sell_price'])
-                tempset = set(stock_dic.keys()) - tempset
-                if (len(stock_dic) == 5) and (len(tempset) == 0):
-                    # 用户的买卖标志不是buy 或sell
-                    if not stock_dic['buy_sell'] in ['buy', 'sell']:
-                        return "buy_sell attribute must be buy or sell!"
-                    # 用户的股票代码不是数字，尚未判断是否有效:是否是六位，是否存在此代码
-                    if not str(stock_dic['code']).isdigit():
-                        return "code attribute must be positive number!"
-                    # 股数如果不为average_all，检查是否是100 的倍数
-                    if stock_dic['volume'] != 'average_all':
-                        if not str(stock_dic['volume']).isdigit():  # 设置的字符串不是正整数
-                            return "volume must be positive int!"
-                        elif (int(str(stock_dic['volume'])) % 100) != 0:  # 判断是否是100 的倍数
-                            return "volume must be multiple of 100!"
-                    # buy_price是否是''or 浮点数
-                    if stock_dic['buy_price'] != '':
-                        if self.isnumber(str(stock_dic['buy_price'])) is False:
-                            return "buy_price must be positive float!"
-                    # sell_price是否是'' or 浮点数
-                    if stock_dic['sell_price'] != '':
-                        if self.isnumber(str(stock_dic['sell_price'])) is False:
-                            return "sell_price must be positive float!"
-                else:
-                    return "stock_dic must be five normal elements!"
-        # 数据暂时没有问题，json编码提交
-        jdata = json.dumps(self.send_dic)  # 对数据进行json格式化编码
-        # print jdata
-        jdata = urllib.urlencode({'tradeinfo': jdata})  # urlencode编码
-        req = urllib2.Request(self.url, jdata)  # 生成页面请求的完整数据
+    # 发起交易请求
+    @staticmethod
+    def http_post(send_dic, urlencode_name, url):
+        jdata = json.dumps(send_dic)  # json格式化编码
+        # print(jdata)
+        # exit()
+        jdata = urllib.urlencode({urlencode_name: jdata})  # urlencode编码
+        req = urllib2.Request(url, jdata)  # 生成页面请求的完整数据
         res = urllib2.urlopen(req)  # 发送页面请求
         temp_res = res.read()  # 返回结果，把list结果处理为字符串显示
-        try:
-            resls = json.loads(temp_res)
-            res_str = ""
-            for x in resls:
-                res_str = res_str + x + '\n'
-        except:
-            res_str = temp_res
-        return res_str
+        return temp_res
 
-if __name__  == '__main__':
+
+if __name__ == '__main__':
     trade = Trade()
-    print trade.isnumber(10)
+    print "".isdigit()
