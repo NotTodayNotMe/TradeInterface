3dec872411c840cf3cdfffe21630861d9e03fe09
cb_Lian <787162506@qq.com>
2018-04-13 11:18:08 +0800

    删除买卖函数中的买卖类型，限制有效期限，增加用户回测子账号的创建 删除 修改

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from setuptools import setup, find_packages
 setup(
     name = "TradeInterface",
 	py_modules = ['TradeInterface'],
-    version = "0.2.1",
+    version = "0.2.2",
     keywords = ("pip", "TradeInterface","trade",'haizhi'),
     description = "a interface of simulate-trading stocks",
     long_description = "a interface of simulate-trading stocks",
